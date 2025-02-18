01852f057903936995b1e8d7fb431e11aad1d2db
Haeun Lee <hlee110123@gmail.com>
2025-02-18 16:12:00 -0500

    test

--- R/hello.R
+++ R/hello.R
@@ -21,6 +21,7 @@ build_atc_query <- function(schema, atc_code) {
   schema <- gsub("[^a-zA-Z0-9_]", "", schema)
   atc_code <- gsub("[^A-Z]", "", atc_code)
 
+  # SQL Server compatible query
   sprintf("
   WITH atc_concepts AS (
       SELECT
@@ -75,7 +76,7 @@ build_atc_query <- function(schema, atc_code) {
       category_prescriptions,
       total_prescriptions,
       ROUND((CASE WHEN total_prescriptions = 0 THEN 0
-            ELSE (CAST(category_prescriptions AS FLOAT) / total_prescriptions) * 100 END), 2) as percentage_of_total
+            ELSE (CAST(category_prescriptions AS FLOAT) / CAST(total_prescriptions AS FLOAT)) * 100 END), 2) as percentage_of_total
   FROM
       prescription_stats",
           schema, atc_code, schema, schema, schema, schema, schema)
